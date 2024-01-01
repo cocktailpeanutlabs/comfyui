5658512cd2bddb3b7137f0bd6a3e9658ca4fc3bd
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-01 13:47:32 -0500

    download sdxl-turbo by default

--- pinokio.js
+++ pinokio.js
@@ -36,7 +36,7 @@ module.exports = {
         icon: "fa-solid fa-download",
         text: "Download Models",
         menu: [
-          { text: "SDXL Turbo", icon: "fa-solid fa-download", href: "download-turbo.json", },
+          { text: "SDXL", icon: "fa-solid fa-download", href: "download-sdxl.json", },
           { text: "Stable Video XT", icon: "fa-solid fa-download", href: "download-svd-xt.json", },
           { text: "Stable Video", icon: "fa-solid fa-download", href: "download-svd.json", },
           { text: "LCM LoRA", icon: "fa-solid fa-download", href: "download-lcm-lora.json", }
